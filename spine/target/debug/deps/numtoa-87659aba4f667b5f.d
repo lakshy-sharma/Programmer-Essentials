e1bd33199f749c5c19df2c227091b8999a0eae94
lakshy <lakshy-sharma@users.noreply.github.com>
2022-12-17 18:37:27 +0530

    Updated to ignore target folder.

--- spine/target/debug/deps/numtoa-87659aba4f667b5f.d
+++
@@ -1,5 +0,0 @@
-/media/lakshy/Lakshy's Files/Workstation/programmer_essentials/spine/target/debug/deps/numtoa-87659aba4f667b5f.rmeta: /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/numtoa-0.1.0/src/lib.rs
-
-/media/lakshy/Lakshy's Files/Workstation/programmer_essentials/spine/target/debug/deps/numtoa-87659aba4f667b5f.d: /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/numtoa-0.1.0/src/lib.rs
-
-/home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/numtoa-0.1.0/src/lib.rs:
--- spine/target/debug/deps/unicode_segmentation-dc387f6a1c9906b1.d
+++
@@ -1,9 +0,0 @@
-/home/lakshy/Data/projects/programmer-essentials/spine/target/debug/deps/unicode_segmentation-dc387f6a1c9906b1.rmeta: /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/lib.rs /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/grapheme.rs /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/sentence.rs /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/word.rs /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/tables.rs
-
-/home/lakshy/Data/projects/programmer-essentials/spine/target/debug/deps/unicode_segmentation-dc387f6a1c9906b1.d: /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/lib.rs /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/grapheme.rs /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/sentence.rs /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/word.rs /home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/tables.rs
-
-/home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/lib.rs:
-/home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/grapheme.rs:
-/home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/sentence.rs:
-/home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/word.rs:
-/home/lakshy/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-segmentation-1.9.0/src/tables.rs:
